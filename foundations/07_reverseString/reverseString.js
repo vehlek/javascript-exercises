dbc3a7aad0de7b444d8b68d2f1b52967d622464b
vehlek <vehlek@gmail.com>
2026-05-11 16:06:35 -0400

    Add (poorer) solution to reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let reverse = "";
 
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str.at(i);
+  }
+
+  return reverse;
 };
 
 // Do not edit below this line
