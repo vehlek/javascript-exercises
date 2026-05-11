6cd2f1dda2f6d7c60e1953873562e387ac9d94e5
vehlek <vehlek@gmail.com>
2026-05-11 15:53:28 -0400

    Add solution to repeatStrings exercise
    Also updates package-lock.json, which is hella files. Not experienced
    enough to know what this is yet. Sounds like I should have used
    "npm ci" instead of "npm install" when messing with jest.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0) return "ERROR";
+  let newStr = "";
+  for (let i = 0; i < num; i++) {
+    newStr += str;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
