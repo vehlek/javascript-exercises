9624c96474f756327af704e9507bd3004249a035
vehlek <vehlek@gmail.com>
2026-05-12 14:44:25 -0400

    Add solution to tempConversion exercise
    My solution assigned the formula to a named value and then returned
    that, I think my toFixed() method is more readable than using
    Math.round() * 10 / 10. I actually prefer my solution to the one
    provided.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celcius = ((fahr - 32) * 5/9);
+  return Number(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fahrenheit = (cel * 9/5 + 32);
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
