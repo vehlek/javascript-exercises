d80a876c5dc08504cfb3bf144e6f18ac3c04482e
vehlek <vehlek@gmail.com>
2026-05-12 13:46:25 -0400

    Add solution to removeFromArray exercise
    My solution is a lot more expensive since it relies on splicing.
    Taking note of how the given solution used rest paraments instead of
    the argument object.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+  let newArr = arr.slice();
+
+  for (let i = 1; i < arguments.length; i++) {
+    for (let j = 0; j < newArr.length; j++) {
+      if (newArr[j] === arguments[i]) {
+        newArr.splice(j, 1);
+        j--;
+      }
+    }
+  }
+
+  return newArr;
 };
 
 // Do not edit below this line
