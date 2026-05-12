2d72b3c416d4c6e258f3677452c8d1bce7781b8e
vehlek <vehlek@gmail.com>
2026-05-12 14:16:32 -0400

    Add solution for sumAll exercise
    One lime of my code is redundant, and I should have used "sum" instead
    of "total" for the result, but otherwise my solution was almost the
    exact same as the given one. Pleased.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+// check if the arguments are both positive numbers
+// if false, return error
 
+// check if argument one is greater than argument two
+// if so, swap their values
+// start a loop where i is equal to argument one's value
+// continue the loop until i is greater than argument two's value
+// within the loop, sum i
+// return the sum
+
+const sumAll = function(a, b) {
+  // I'll keep this here, but as the solution shows, Number.isInteger()
+  // renders typeof() redundant
+  if (typeof(a) !== "number" || typeof(b) !== "number" ||
+      !Number.isInteger(a) || !Number.isInteger(b) ||
+      a < 0 || b < 0) {
+    return "ERROR";
+  }
+
+  if (a > b) {
+    [a, b] = [b, a];
+  }
+
+  let total = 0;
+  for (let i = a; i <= b; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
